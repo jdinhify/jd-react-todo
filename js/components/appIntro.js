b6f6712d0d0a2fc353a3bdbabe7e345b3297ce87
JD <me@jdinh.life>
2016-06-09 22:08:49 +1000

    typo

--- js/components/appIntro.js
+++ js/components/appIntro.js
@@ -4,7 +4,7 @@ const AppIntro = () =>
     <div
         className='intro align-center'>
         <h1>Simple todo list by JD</h1>
-        <p>Please note this app utilises localStorage (if available) to make data persistent</p>
+        <p>Please note this app utilises localStorage (if available) for data persistence</p>
         <p>Browsers support list can be found here: <a href="http://caniuse.com/#search=localstorage">http://caniuse.com/#search=localstorage</a></p>
     </div>;
 ;
