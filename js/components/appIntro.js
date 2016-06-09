4175a4bbd7f8f5ae293a0831d44c522195e207c9
JD <me@jdinh.life>
2016-06-09 21:56:23 +1000

    colour contrast fixed

--- js/components/appIntro.js
+++ js/components/appIntro.js
@@ -5,7 +5,7 @@ const AppIntro = () =>
         className='intro align-center'>
         <h1>Simple todo app by JD</h1>
         <p>Please note this app utilises localStorage (if available) to make data persistent</p>
-        <p>Browser support list can be found here: <a href="http://caniuse.com/#search=localstorage">http://caniuse.com/#search=localstorage</a></p>
+        <p>Browsers support list can be found here: <a href="http://caniuse.com/#search=localstorage">http://caniuse.com/#search=localstorage</a></p>
     </div>;
 ;
 
